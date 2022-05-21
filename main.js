2b228f888a407df5047b93630818914a27c6fab9
swapnilkrish <86659449+swapnilkrish@users.noreply.github.com>
2022-05-21 15:19:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/y6Pn24CX/580b57fbd9996e24bc43bed5.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
+function modelLoaded() {
+    console.log('poseNet Is Initialized');
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX - 10, noseY - 10, 30, 30);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
